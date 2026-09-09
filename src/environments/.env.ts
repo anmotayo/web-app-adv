798bca66ec602fc1aa61d5512aeee2f70adb87c3
eboka.kevin <kevin.eboka@advancly.com>
2026-09-09 15:01:12 +0100

    fix ui issue for payment types

--- src/environments/.env.ts
+++ src/environments/.env.ts
@@ -2,8 +2,8 @@
 /* tslint:disable */
 export default {
   'mifos_x': {
-    'version': '260817',
-    'hash': 'dbc70a7b3'
+    'version': '260909',
+    'hash': 'g58226f2fb'
   },
   'allow_switching_backend_instance': true
 };
